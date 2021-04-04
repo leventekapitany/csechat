14e533ce9b42f04f6e353e12b039a892a86133d7
kapitanylevente <kapitanylevente04@gmail.com>
2021-04-04 19:44:08 +0200

    unique username

--- models.js
+++ models.js
@@ -1,12 +1,7 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        index: {
-            unique: true
-        }
-    },
+    _id : String,
     password: String
 })
 
